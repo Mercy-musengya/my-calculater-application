f1506decb3debf8a5e76a02debc1ccacd5f11f5a
mercy-musengya <musenyamercy324@gmail.com>
2024-07-11 03:15:22 -0700

    my calculator app

---
+++ calculater.cpp
@@ -0,0 +1,3 @@
+#include <iostream>  // For input-output operations
+#include <cmath>     // For advanced math functions
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
